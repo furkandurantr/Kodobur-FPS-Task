5665c91f4dbf33cefe958b8d3fa7c9b85573b8c7
Durkan <furkandurantr@gmail.com>
2024-04-19 19:58:45 +0300

    Jump and Sprint

---
+++ Kodobur FPS Case/Assets/MyNotes.cs
@@ -0,0 +1,35 @@
+/*
+
+Kodobur FPS Case notes & to-do
+
+A game with FPS movement and upgrade selection mechanic
+
+Player
++ Character Movement & Control
++ Sprint w Lshift
++ Jump w Space
+- Jump will be limited w a max height (or with a value i guess?)
+
+Movement Fixes
+- Coyote Time
++ Jump Buffering
+
+
+Gun
+- Gun Obj
+- Damage
+- Ammo/MaxAmmo
+- Piercing
+
+Gun & Player
+- Shown at screen
+- Raytracing 
+- Fix view point and gun point
+
+Basics
+- HP/MaxHP
+- Ammo
+- Kill Score
+- Exp/Level
+
+*/
--- Kodobur FPS Case/Assets/PlayerMovement.cs
+++ Kodobur FPS Case/Assets/PlayerMovement.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     public CharacterController controller;
-
     public float speed = 12f;
+    public float sprintSpeed = 18f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public float bufferDistance = 4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+    bool isJumpBuffer;
+    public float jumpHeight = 3f;
+
+    bool jumpBuffer = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +31,37 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isJumpBuffer = Physics.CheckSphere(groundCheck.position, bufferDistance, groundMask);
+
+        if (Input.GetButtonDown("Jump") && isJumpBuffer) 
+        {
+            jumpBuffer = true;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2;
+        }
+
+        if (jumpBuffer && isGrounded)   
+        {
+            jumpBuffer = false;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        //Sprinting to int
+        int sprinting = Input.GetButton("Sprint") ? 1 : 0;
+
+        //Find max speed
+        float mySpeed = Mathf.Max(speed,  sprinting * sprintSpeed);
+        controller.Move(move * mySpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
     }
 }
